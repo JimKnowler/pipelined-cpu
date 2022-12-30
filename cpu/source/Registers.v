3eda67d04091bb976121ec4b9391f051f81c8d9b
Jim Knowler <jim.knowler2@gmail.com>
2022-12-30 16:27:58 +0000

    Registers: first pass of register file with single write port and 2 read ports

--- cpu/source/Registers.v
+++ cpu/source/Registers.v
@@ -1,10 +1,47 @@
 module Registers(
-    /* verilator lint_off UNUSED */
     input i_clk,
-    input i_reset_n
-    /* verilator lint_on UNUSED */
+    input i_reset_n,
+
+    input i_we,
+    input [3:0] i_ws,
+    input [31:0] i_wd,
+
+    input [3:0] i_rs1,
+    input [3:0] i_rs2,
+
+    output [31:0] o_rd1,
+    output [31:0] o_rd2
 );
 
+reg [31:0] r_registers [15:0];
 
+reg [31:0] r_rd1;
+reg [31:0] r_rd2;
+
+always @(*)
+begin
+   r_rd1 = r_registers[i_rs1];
+   r_rd2 = r_registers[i_rs2]; 
+end
+
+integer i;
+
+always @(posedge i_clk or negedge i_reset_n)
+begin
+    if (!i_reset_n)
+    begin
+        begin
+            for (i=0; i<16; i=i+1)
+                r_registers[i] <= 0;
+        end
+    end
+    else if (i_we)
+    begin
+        r_registers[i_ws] <= i_wd;
+    end
+end
+
+assign o_rd1 = r_rd1;
+assign o_rd2 = r_rd2;
 
 endmodule
--- cpu/test/Registers.test.cpp
+++ cpu/test/Registers.test.cpp
@@ -11,15 +11,11 @@ namespace {
     class Registers : public ::testing::Test {
     public:
         void SetUp() override {
-            //testBench.setClockPolarity(1);
-            //testBench.core().i_clk_en = 1;
+            auto& core = testBench.core();
+            core.i_we = 0;
         }
         
         void TearDown() override {
-        }
-
-        void HelperReset() {
-            testBench.reset();
         }
 
         RegistersTestBench testBench;
@@ -28,5 +24,99 @@ namespace {
 
 TEST_F(Registers, ShouldConstruct) {
 
+}
+
+TEST_F(Registers, ShouldReset) {
+    testBench.reset();
+
+    auto& core = testBench.core();
+
+    for (int i=0; i<16; i++) {
+        core.i_rs1 = i;
+        core.i_rs2 = i;
+
+        core.eval();
+
+        EXPECT_EQ(0, core.o_rd1);
+        EXPECT_EQ(0, core.o_rd2);
+    }
+}
+
+TEST_F(Registers, ShouldWriteToSingleRegister) {
+    auto& core = testBench.core();
+
+    const uint32_t TestValue = 0x12345678;
+    const uint8_t TestRegister = 12;
+
+    core.i_we = 1;
+    core.i_wd = TestValue;
+    core.i_ws = TestRegister;
+    testBench.tick();
+
+    core.i_we = 0;
+
+    for (int i=0; i<16; i++) {
+        core.i_rs1 = i;
+        core.i_rs2 = i;
+
+        core.eval();
+
+        if (i == TestRegister) {
+            EXPECT_EQ(TestValue, core.o_rd1);
+            EXPECT_EQ(TestValue, core.o_rd2);
+        } else {
+            EXPECT_EQ(0, core.o_rd1);
+            EXPECT_EQ(0, core.o_rd2);
+        }
+    }
+}
+
+TEST_F(Registers, ShouldWriteToAllRegisters) {
+    auto& core = testBench.core();
+
+    core.i_we = 1;
+
+    for (int i=0; i<16; i++) {
+        core.i_ws = i;
+        core.i_wd = i+1;
+        testBench.tick();
+    }
+
+    core.i_we = 0;
+
+    for (int i=0; i<16; i++) {
+        core.i_rs1 = i;
+        core.i_rs2 = i;
+
+        core.eval();
+
+        EXPECT_EQ(i+1, core.o_rd1);
+        EXPECT_EQ(i+1, core.o_rd2);
+    }
+}
+
+
+TEST_F(Registers, ShouldReadConcurrentlyFromDifferentRegisters) {
+    auto& core = testBench.core();
+
+    core.i_we = 1;
+
+    for (int i=0; i<16; i++) {
+        core.i_ws = i;
+        core.i_wd = i+1;
+        testBench.tick();
+    }
+
+    core.i_we = 0;
+
+    for (int i=0; i<8; i++) {
+        core.i_rs1 = 2*i;
+        core.i_rs2 = (2*i) + 1;
+
+        core.eval();
+
+        EXPECT_EQ((2*i)+1, core.o_rd1);
+        EXPECT_EQ(((2*i)+1)+1, core.o_rd2);
+    }
 }
 
